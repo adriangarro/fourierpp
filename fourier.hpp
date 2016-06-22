c08bcb7f4e3f28ab3bb80f99844d5f37f64aa087
Adrián Garro <adriangarro@icloud.com>
2016-06-21 19:16:11 -0600

    Minor changes

--- fourier.hpp
+++ fourier.hpp
@@ -1,7 +1,9 @@
-/* File: fourier.hpp
- * Author: Elberth Adrian Garro Sanchez
- * Utility: Implements Discrete Fourier Transform Algorithms
- * Built with ISO C++14
+/* 
+    Implements Discrete Fourier Transform Algorithms
+    
+    Built with C++14
+    
+    (c) E. Adrian Garro S. Costa Rica Institute of Technology.
  */
 
 #include <cmath>
@@ -23,7 +25,8 @@ namespace fourierpp {
         }
     }
     
-    void adjust_zero_precision(std::complex<double>& point)  // close to zero? you're zero
+    void adjust_zero_precision(std::complex<double>& point)  
+        // close to zero? you're zero
     {
       auto real_part = point.real();
       auto imag_part = point.imag();
@@ -37,23 +40,28 @@ namespace fourierpp {
       }
     }
     
-    template <class signal_vector>  // points of signal close to zero become zero
-    void adjust_result(signal_vector& result) 
+    template <class signal_vector>
+    void adjust_result(signal_vector& result)
+        // points of signal close to zero become zero 
     {
-      for (auto& point : result) {
-        adjust_zero_precision(point);
-      }
+        for (auto& point : result) {
+            adjust_zero_precision(point);
+        }
     }
     
     template <class signal_vector>
-    signal_vector sdft(signal_vector& data) // Simple DFT (not in-place)
+    signal_vector sdft(signal_vector& data) 
+        // Simple DFT (not in-place)
     {
         auto data_size = data.size();
         signal_vector result(data_size);
-        for (auto freq = 0; freq < data_size; ++freq) {  // for every frequency...
-            for (auto t = 0; t < data_size; ++t) {  // for every point in time...
+        // for every frequency...
+        for (auto freq = 0; freq < data_size; ++freq) {
+            // for every point in time...
+            for (auto t = 0; t < data_size; ++t) {  
                 auto twiddle_factor = std::polar(1.0, -2 * pi * freq * t / data_size);
-                auto result_point = data[t] * twiddle_factor;  // datapoint * e^(-i*2*pi*f)
+                // datapoint * e^(-i*2*pi*f)
+                auto result_point = data[t] * twiddle_factor;
                 result[freq] += result_point;
             }
         }
@@ -63,19 +71,17 @@ namespace fourierpp {
     
     template <class signal_vector, typename size>
     void partition_by_index(signal_vector& data, size data_size, 
-                               signal_vector& odd, signal_vector& even) 
+                            signal_vector& odd, signal_vector& even) 
     {
         for (auto index = 0; index < data_size; ++index) {
-            if ((index % 2) != 0) {
-                odd.push_back(data.at(index));
-            } else {
-                even.push_back(data.at(index));
-            }
+            if ((index % 2) != 0) odd.push_back(data.at(index));
+            else even.push_back(data.at(index));
         }
     }
     
     template <class signal_vector>
-    void aux_fft(signal_vector& data)  // Cooley–Tukey FFT (in-place)
+    void aux_fft(signal_vector& data)  
+        // Cooley–Tukey FFT (in-place)
     {
         auto data_size = data.size();
         if (data_size == 1) return;
@@ -95,7 +101,8 @@ namespace fourierpp {
     }
     
     template <class signal_vector>
-    signal_vector fft(signal_vector& data)  // Cooley–Tukey FFT (not in-place)
+    signal_vector fft(signal_vector& data)  
+        // Cooley–Tukey FFT (not in-place)
     {
         signal_vector result(data);
         aux_fft(result);
@@ -103,7 +110,8 @@ namespace fourierpp {
     }
     
     template <class signal_vector>
-    signal_vector idft(signal_vector& data)  // Inverse DFT (not in-place)
+    signal_vector idft(signal_vector& data)  
+        // Inverse DFT (not in-place)
     {
         auto data_size = data.size();
         signal_vector result(data_size);
@@ -116,7 +124,7 @@ namespace fourierpp {
                 auto result_point = data[freq] * twiddle_factor;
                 result[t] += result_point;
             }
-        result[t] /= data_size;
+            result[t] /= data_size;
         }
         adjust_result(result);
         return result;
